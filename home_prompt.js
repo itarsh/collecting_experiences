96acf6f3a88f782b9c0c3b69910020be373dfd63
oliviacopping <61801860+oliviacopping@users.noreply.github.com>
2020-03-10 22:01:14 +0000

    Add files via upload

---
+++ home_prompt.js
@@ -0,0 +1,12 @@
+alert("Welcome to Collecting Experiences, thank you for visiting our site!");
+
+window.onscroll = function() {myFunction()};
+  var nav = document.getElementById("nav");
+  var sticky = nav.offsetTop;
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      nav.classList.add("sticky")
+    } else {
+      nav.classList.remove("sticky");
+    }
+  }

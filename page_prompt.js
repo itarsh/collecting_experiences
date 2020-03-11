6382d8bb30bbd2323803b044574f6a8942dc8285
itarsh <59577237+itarsh@users.noreply.github.com>
2020-03-11 13:01:54 +0000

    Attempt at js without alert for food page

---
+++ page_prompt.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {myFunction()};
+  var nav = document.getElementById("nav");
+  var sticky = nav.offsetTop;
+  function myFunction() {
+    if (window.pageYOffset >= sticky) {
+      nav.classList.add("sticky")
+    } else {
+      nav.classList.remove("sticky");
+    }
+  }
+
+function Subscribe() {
+  alert("Thank you for subscribing to Collecting Experiences!"); 
+}
